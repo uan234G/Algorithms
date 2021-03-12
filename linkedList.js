fd9f27be3e95cd50afb04578455d044a7e63cde6
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-11 19:40:07 -0800

    Update linkedList.js
    adding size of list
    get last node in linkedlist

--- linkedList.js
+++ linkedList.js
@@ -9,12 +9,32 @@ class linkedList {
     constructor(head = null) {
         this.head = head
     }
+    size() {
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }
+    getLast() {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode;
+    }
 }
 
 let node1 = new Node("peter");
 let node2 = new Node("miles");
 node1.next = node2
 
-let list = new linkedList(node1)
+let list = new linkedList(node1);
+console.log(list.size()); //return 2
+console.log(list.getLast()); //return {last node| val + next}
+console.log(list.getLast().val); //return miles
 
-console.log(list.head.next.val) // return miles
