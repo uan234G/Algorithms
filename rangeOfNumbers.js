6ad0c281018ec207259647e8d712cc2a6b3efc23
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-11 20:57:17 -0800

    Create rangeOfNumbers.js
    trying out recursion

---
+++ rangeOfNumbers.js
@@ -0,0 +1,13 @@
+// USING RECURSION
+
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+        return [startNum];
+    } else {
+        var numbers = rangeOfNumbers(startNum, endNum - 1);
+        numbers.push(endNum);
+        return numbers;
+    }
+}
+
+console.log(rangeOfNumbers(1, 90));
