de2970b3718797c38305cf526a2ab41465b707b4
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-22 20:33:59 -0700

    Create countingSort.js

---
+++ countingSort.js
@@ -0,0 +1,21 @@
+function countingSort(arr, min, max) {
+    let i = min,
+        j = 0,
+        len = arr.length,
+        count = [];
+    for (i; i <= max; i++) {
+        count[i] = 0;
+    }
+    for (i = 0; i < len; i++) {
+        count[arr[i]] += 1;
+    }
+    for (i = min; i <= max; i++) {
+        while (count[i] > 0) {
+            arr[j] = i;
+            j++;
+            count[i]--;
+        }
+    }
+    return arr;
+};
+console.log(countingSort([1, 5, 4, 2, 9, 6, 8, 7, 10, 0], 0, 10));
