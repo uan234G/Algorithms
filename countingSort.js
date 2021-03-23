37bdc2e5504c1376d492d13b3f9c5a2af3d1b055
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-22 20:34:46 -0700

    Update countingSort.js

--- countingSort.js
+++ countingSort.js
@@ -19,3 +19,10 @@ function countingSort(arr, min, max) {
     return arr;
 };
 console.log(countingSort([1, 5, 4, 2, 9, 6, 8, 7, 10, 0], 0, 10));
+/*
+[
+    0, 1, 2, 4,  5,
+    6, 7, 8, 9, 10
+]
+*/
+
