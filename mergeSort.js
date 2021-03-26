4dce5725d45576bd388b7769c5f0c3f0b229eb25
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-25 19:54:20 -0700

    Update mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,13 +1,8 @@
 /*
     1. create a merge function to merge two already sorted arrays into a single
         sorted array.
-        - you do NOT need to work in place, ok to use a new array
     2. create mergeSort function to sort the provided array
-        - split the array in half and recursively merge the halves using the
-        previously created merge function.
-        - splitting of arrays stops when array can no longer be split.
-        - an array of 1 item is by definition sorted, so two arrays of 1 item
-        can therefore be merged into a sorted array.
+
 */
 function merge(leftarr, rightarr) {
     result = [];
@@ -40,3 +35,30 @@ const sortedA4 = [1, 2, 4, 5, 6, 9];
 const sortedB4 = [3, 7, 8, 10];
 const test1 = merge(sortedA4, sortedB4)
 console.log(test1);
+/*
+[
+    1, 2, 3, 4,  5,
+    6, 7, 8, 9, 10
+]
+*/
+
+function mergeSort(nums) {
+    if (nums.length === 1) {
+        return nums;
+    }
+
+    const middle = Math.floor(nums.length / 2); // middle item
+    const left = nums.slice(0, middle); // items on the left side
+    const right = nums.slice(middle); // items on the right side
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const test2 = mergeSort([2, 5, 4, 78, 6, 1, 2, 0, 9, 3]);
+console.log(test2);
+/*
+[
+    0, 1, 2, 2, 3,
+    4, 5, 6, 9, 78
+]
+*/
