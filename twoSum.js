1c2f19e1ad6c7eb53796781d9d369c3f5680889a
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-03-28 20:43:09 -0700

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -4,7 +4,7 @@ function twoSum(nums, target) {
     const idx = {};
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
-        diff = target - num;
+        const diff = target - num;
 
         if (idx.hasOwnProperty(diff)) {
             const appendIdk = idx[diff];
@@ -22,3 +22,20 @@ console.log(twoSum(nums1, targetSum));
 // expected1 answer = [0, 2];
 // // Order doesn't matter. Explanation: nums[0] + nums[2] = 2 + 7 = 9
 
+/**
+ * - Time: O(n^2) quadratic.
+ * - Space: O(1) constant.
+ */
+function twoSumSpaceOptimized(nums, targetSum) {
+    const indexes = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; ++j) {
+            if (nums[i] + nums[j] === targetSum) {
+                indexes.push(i, j);
+                break;
+            }
+        }
+    }
+    return indexes;
+}
