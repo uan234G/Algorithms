73df69bf2e768fbc4dbf31ef308147872e93485f
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-04-05 21:11:41 -0700

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,36 +1,38 @@
 // sampling ---> https://dev.to/ christinamcmahon
-// // HELPERS
-// function swap(arr, a, b) { //simple swap
-//     let temp = arr[a];
-//     arr[a] = arr[b];
-//     arr[b] = temp;
-// }
-// const Compare = { // compareing values
-//     lessThan: -1,
-//     biggerThan: 1,
-// }
-// function returnCompare(a, b) {
-//     if (a === b) {
-//         return 0;
-//     }
-//     return a < b ? Compare.lessThan : Compare.biggerThan;
-// }
-// // BUBBLE SORTING
-// function bubbleSort(arr, compare = returnCompare) {
-//     // const length = arr.length
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr.length - 1 - i; j++) {
-//             if (compare(arr[j], arr[j + 1]) === Compare.biggerThan) {
-//                 swap(arr, j, j + 1);
-//             }
-//         }
-//     }
-//     return arr;
+// HELPERS
+function swap(arr, a, b) { //simple swap
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+const Compare = { // compareing values
+    lessThan: -1,
+    biggerThan: 1,
+}
+function returnCompare(a, b) {
+    if (a === b) {
+        return 0;
+    }
+    return a < b ? Compare.lessThan : Compare.biggerThan;
+}
+// BUBBLE SORTING
+function bubbleSort(arr, compare = returnCompare) {
+    // const length = arr.length
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (compare(arr[j], arr[j + 1]) === Compare.biggerThan) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
 
-// }
+}
+var y = [6, 14, 8, 67, 67, 32, 1, 0, 3, 5, 6, 7, 2, 1, 76, 7, 8, 6, 5, 3, 89, 23, 21];
+console.log(bubbleSort(y));
 
 // simple
-function bubbleSort(arr) {
+function bubbleSort2(arr) {
     let arrLength = arr.length;
     for (let i = 0; i < arrLength; i++) {
         for (let j = 0; j < arrLength; j++) {
@@ -45,4 +47,4 @@ function bubbleSort(arr) {
 };
 
 var x = [6, 14, 8, 67, 67, 32, 1, 0, 3, 5, 6, 7, 2, 1, 76, 7, 8, 6, 5, 3, 89, 23, 21];
-console.log(bubbleSort(x));
+console.log(bubbleSort2(x));
