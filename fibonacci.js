9ceb9ba6e3ba81757a3ad7cc25c2bdb1a2e18f61
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-04-16 21:40:50 -0700

    Update fibonacci.js
    update

--- fibonacci.js
+++ fibonacci.js
@@ -18,3 +18,16 @@ const num5 = 7;  //returns 13
 
 console.log(fibonacci(num));
 
+// memoization method
+
+function fib(n, memo = {}) {
+    if (n in memo) {
+        return memo[n]
+    }
+    if (n <= 2) {
+        return 1
+    }
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n]
+}
+console.log(fib(num));  //returns 21
