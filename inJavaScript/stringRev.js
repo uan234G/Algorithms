01d2db2baae9cb50f06a0076d4bf13a61b03a5df
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-05-12 19:26:04 -0700

    Update stringRev.js

--- inJavaScript/stringRev.js
+++ inJavaScript/stringRev.js
@@ -16,4 +16,13 @@ function reverseString(string) {
 const test1 = reverseString("hello");
 console.log(test1);
 
-// 
+// Method 2 
+// Use split() inbuilt function in JavaScript to split string into array of characters.
+// Use reverse() function in JavaScript to reversal the array of characters
+// Use join() function in JavaScript to join the elements of an array into a string.
+function newRevString(str) {
+    return str.split('').reverse().join('');
+}
+
+const test2 = newRevString("random words");
+console.log(test2);
