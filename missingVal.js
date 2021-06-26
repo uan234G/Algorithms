a791f2229fb73f54f68f9c71f4b34be6f8a0f8f1
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-06-25 21:13:52 -0700

    Create missingVal.js

---
+++ missingVal.js
@@ -0,0 +1,24 @@
+// /* 
+//   Missing Value
+//   You are given an array of length N that contains, in no particular order,
+//   integers from 0 to N . One integer value is missing.
+//   Quickly determine and return the missing value.
+// */
+
+const nums1 = [3, 0, 1];
+const expected1 = 2;
+
+const nums2 = [3, 0, 1, 2];
+const expected2 = null;
+// Explanation: nothing is missing
+
+// /* 
+//   Bonus: now the lowest value can now be any integer (including negatives),
+//   instead of always being 0. 
+// */
+
+const nums3 = [2, -4, 0, -3, -2, 1];
+const expected3 = -1;
+
+const nums4 = [5, 2, 7, 8, 4, 9, 3];
+const expected4 = 6;
