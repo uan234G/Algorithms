9ce616a2900c34862e04e30d5b062384f677a9fc
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-06-26 23:03:56 -0700

    Update dropList.js

--- dropList.js
+++ dropList.js
@@ -25,3 +25,29 @@ const nums3 = [...nums1];
 const callback3 = (elem) => false;
 const expected3 = [];
 
+
+/**
+ * Removes every element in the array, starting from idx 0 until the callback
+ * function returns true when passed the iterated element.
+ * - Time: O(n) linear. n = arr.length.
+ * - Space: O(n) linear, due to .splice creating a new array of n length at
+ *    most.
+ */
+
+
+function dropIt(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            arr.splice(0, i);
+            return arr;
+        }
+    }
+    arr.splice(0, arr.length);
+    return arr;
+}
+
+function dropItFunctional(arr, callback) {
+    const delCount = arr.findIndex((elem) => callback(elem) === true);
+    arr.splice(0, delCount === -1 ? arr.length : delCount);
+    return arr;
+}
