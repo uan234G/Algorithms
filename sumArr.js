7a47d79ca3cb748dc5f4a74691d6b416dbd71c08
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-06-27 23:07:56 -0700

    Update sumArr.js

--- sumArr.js
+++ sumArr.js
@@ -4,3 +4,13 @@
 
 // const nums1 = [1, 2, 3];
 // const expected1 = 6;
+
+
+function sumArray(nums, i = 0) {
+    if (i === nums.length) {
+        return 0;
+    }
+    return nums[i] + sumArray(nums, i + 1);
+}
+let nums1 = [1, 2, 3];
+console.log(sumArray(nums1))
