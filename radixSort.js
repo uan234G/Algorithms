bcc1f33fea943f4ccbc7c34447504e90bf08121e
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-06-28 20:30:43 -0700

    Update radixSort.js

--- radixSort.js
+++ radixSort.js
@@ -2,3 +2,18 @@
 
 // src=https://reactgo.com/radix-sort-algorithm-javascript/
 
+// setup
+function getPosition(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+// get num with most digits
+function getMax(arr) {
+    let max = 0;
+    for (let num of arr) {
+        if (max < num.toString().length) {
+            max = num.toString().length;
+        }
+    }
+    return max;
+}
