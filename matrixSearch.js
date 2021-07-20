9185874cbc911ebc0e195fba69d6ef3198034ad6
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-07-19 22:59:36 -0700

    Update matrixSearch.js

--- matrixSearch.js
+++ matrixSearch.js
@@ -1,2 +1,8 @@
 // src=https://bitbucket.org/morleytatro/dojo-algorithms/src/master/chapter-7-arrays-part-2.js
 
+/*
+  Matrix Search
+  Mike digs image recognition and wants to create a JavaScript Imaging Library, just like PIL for Python. He is given 2 different two-dimensional arrays, containing integers between 0 and 65535. Each two-dimensional array represents a gray-scale image, where each integer value is a pixel. The second image might be found somewhere within the larger one. Return whether it is.
+  Given array: [[12,34,45,56], and array: [[67,78], return true . [98,87,76,65],[43,32] ] [56, 67 , 78 ,89] , [54, 43 , 32 ,21] ]
+  Second: Return location of first match found ( [-1,-1] if no match). In example above, return [2,1] .
+*/
